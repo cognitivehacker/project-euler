f0169f2b7cb11b9f14aee7cb1e9d6fdf1593601a
yuri <yurifl03@gmail.com>
2020-05-11 22:03:38 -0300

    wip

--- 3-largest-prime-factor/largest_prime_factor.go
+++ 3-largest-prime-factor/largest_prime_factor.go
@@ -1,13 +1,8 @@
 package largestprime
 
-func isPrime(n int) bool {
-	for j := 2; j < n; j++ {
-		if n%j == 0 {
-			return false
-		}
-	}
-	return true
-}
+// # 3) Largest prime factor
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
 
 // LargestPrimeFactor return the largest prime factor until n
 func LargestPrimeFactor(n int) int {
@@ -20,4 +15,13 @@ func LargestPrimeFactor(n int) int {
 		}
 	}
 	return out
+}
+
+func isPrime(n int) bool {
+	for j := 2; j < n; j++ {
+		if n%j == 0 {
+			return false
+		}
+	}
+	return true
 }
