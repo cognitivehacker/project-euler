045ffeb1f10ad2da1adafb2d3770a6573b59d257
yuri <yurifl03@gmail.com>
2020-05-11 21:55:56 -0300

    wip

---
+++ 5-smallest-multiple/smallest_multiple.go
@@ -0,0 +1,27 @@
+package pkg
+
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10
+// without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+// x = 1
+// y = 2
+//
+func SmallestMultiple(upperBound int) int {
+	win := 0
+	for x := 1; true; x++ {
+		if IsDivisibleByRange(x, upperBound) {
+			return x
+		}
+	}
+	return win
+}
+
+func IsDivisibleByRange(testd int, max int) bool {
+	for y := 1; y < max; y++ {
+		if testd%y != 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ 5-smallest-multiple/smallest_multiple_test.go
@@ -0,0 +1,64 @@
+package pkg
+
+import (
+	"testing"
+)
+
+func TestSmallestMultiple(t *testing.T) {
+	type args struct {
+		a int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Testing for a:10",
+			args: args{a: 10},
+			want: 2520,
+		},
+		{
+			name: "Testing for a:10",
+			args: args{a: 20},
+			want: 232792560,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SmallestMultiple(tt.args.a); got != tt.want {
+				t.Errorf("SmallestMultiple() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsDivisibleByRange(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Testing for a:10 b:2",
+			args: args{a: 2520, b: 10},
+			want: true,
+		},
+		{
+			name: "Testing for a:10 b:2",
+			args: args{a: 2, b: 10},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsDivisibleByRange(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("IsDivisibleByRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
